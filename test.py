5d4b4bed6cfa80b81289624bc8878091d38fcdc3
Jeremydozie <65295271+Jeremydozie@users.noreply.github.com>
2023-09-01 15:09:58 -0500

    action-test

--- test.py
+++ test.py
@@ -1,43 +1,27 @@
-# Save this file as "test_example.py"
+# test_calculator.py
 
-# Import the module or code you want to test
-from my_module import add, subtract
+import calculator
 
-# Define test functions using the "test_" prefix
-# pytest will automatically discover and run these functions
+def test_add():
+    assert calculator.add(2, 3) == 5
+    assert calculator.add(-1, 1) == 0
+    assert calculator.add(0, 0) == 0
 
-def test_addition():
-    result = add(2, 3)
-    assert result == 5
+def test_subtract():
+    assert calculator.subtract(5, 3) == 2
+    assert calculator.subtract(1, -1) == 2
+    assert calculator.subtract(0, 0) == 0
 
-def test_subtraction():
-    result = subtract(5, 2)
-    assert result == 3
+def test_multiply():
+    assert calculator.multiply(2, 3) == 6
+    assert calculator.multiply(-1, 1) == -1
+    assert calculator.multiply(0, 5) == 0
 
-def test_division_by_zero():
-    # This test demonstrates how to use pytest's built-in assertion for exceptions
-    with pytest.raises(ZeroDivisionError):
-        result = divide(1, 0)
+def test_divide():
+    assert calculator.divide(6, 3) == 2
+    assert calculator.divide(10, 2) == 5
 
-# You can also use fixtures for setup and teardown if needed
-# Here's a simple fixture example:
-
-import pytest
-
-@pytest.fixture
-def setup_example():
-    print("Setup for test")
-    # Any setup code can go here, and this fixture can be reused in multiple tests
-    yield
-    print("Teardown after test")
-    # Any teardown code can go here
-
-def test_example_with_fixture(setup_example):
-    # The setup_example fixture will be automatically passed to this test function
-    assert True  # You can replace this with your actual test logic
-
-# To run these tests, simply execute "pytest" in the directory containing this test file:
-# $ pytest
-
-# pytest will discover and execute the test functions, reporting the results.
-
+def test_divide_by_zero():
+    # Test for division by zero, should raise a ValueError
+    with pytest.raises(ValueError):
+        calculator.divide(5, 0)
