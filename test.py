52adec132f6b6f1730bacdccf68123a8403c5c82
Jeremydozie <65295271+Jeremydozie@users.noreply.github.com>
2023-09-01 15:30:49 -0500

    action-test

--- test.py
+++ test.py
@@ -1,27 +1,11 @@
-# test_calculator.py
+# main.py
 
-import calculator
+# Import your custom module
+import my_module
 
-def test_add():
-    assert calculator.add(2, 3) == 5
-    assert calculator.add(-1, 1) == 0
-    assert calculator.add(0, 0) == 0
+# Use functions from my_module
+result_add = my_module.add(5, 3)
+result_subtract = my_module.subtract(10, 2)
 
-def test_subtract():
-    assert calculator.subtract(5, 3) == 2
-    assert calculator.subtract(1, -1) == 2
-    assert calculator.subtract(0, 0) == 0
-
-def test_multiply():
-    assert calculator.multiply(2, 3) == 6
-    assert calculator.multiply(-1, 1) == -1
-    assert calculator.multiply(0, 5) == 0
-
-def test_divide():
-    assert calculator.divide(6, 3) == 2
-    assert calculator.divide(10, 2) == 5
-
-def test_divide_by_zero():
-    # Test for division by zero, should raise a ValueError
-    with pytest.raises(ValueError):
-        calculator.divide(5, 0)
+print("Addition result:", result_add)
+print("Subtraction result:", result_subtract)
