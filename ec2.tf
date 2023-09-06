1fb90bc25b8d6acfbdc8d19f7c35453d3d4d61f2
Jeremydozie <65295271+Jeremydozie@users.noreply.github.com>
2023-09-06 14:43:25 -0500

    terraform-actions

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
-provider "aws" {
-    region = "us-east-1"
-  
-}
-
-resource "aws_instance" "jay_ec2-test" {
-    instance_type = "t2.micro"
-    ami = "ami-053b0d53c279acc90"
-    tags = {
-      Name = "jay-instance"
-    }
-  
-}
+#provider "aws" {
+#    region = "us-east-1"
+#  
+#}
+#
+#resource "aws_instance" "jay_ec2-test" {
+#    instance_type = "t2.micro"
+#    ami = "ami-053b0d53c279acc90"
+#    tags = {
+#      Name = "jay-instance"
+#    }
+#  
+#}
